2849992923b144e6865c73be98e6401693f1dda0
SeolYoungKim <nasur4da@gmail.com>
2023-01-08 22:36:00 +0900

    feat: ClassLoaderTest 수행

--- toby-spring-practice/src/test/java/com/study/java_practice/ClassLoaderTest.java
+++ toby-spring-practice/src/test/java/com/study/java_practice/ClassLoaderTest.java
@@ -7,24 +7,23 @@ import org.junit.jupiter.api.Test;
 public class ClassLoaderTest {
 
     @Test
-    void name() throws Exception {
-        Class<?> classHello = Class.forName("com.study.java_practice.ReflectionTest$Hello");
-        ClassLoader classLoader = classHello.getClassLoader();
-        Class<?> aClass = classLoader.loadClass("com.study.java_practice.ReflectionTest$Hello");
-        log.info("classLoader = {}", classLoader);
-        log.info("classLoader = {}", classLoader.getParent());
-        log.info("classLoader = {}", classLoader.getParent().getParent());
+    void classInitTest() throws Exception {
+        Class<?> classDriverA = Class.forName("com.study.java_practice.ClassLoaderTest$DriverA");
+
+        ClassLoader classLoader = classDriverA.getClassLoader();
+        log.info("ClassLoader 호출 = {}", classLoader);
+        Class<?> classDriverB = classLoader.loadClass("com.study.java_practice.ClassLoaderTest$DriverB");
     }
 
     static class DriverA {
         static {
-            System.out.println("DriverA - 초기화 되었습니다.");
+            log.info("DriverA : 초기화 되었습니다.");
         }
     }
 
     static class DriverB {
         static {
-            System.out.println("DriverB - 초기화 되었습니다.");
+            log.info("DriverB : 초기화 되었습니다.");
         }
     }
 }
